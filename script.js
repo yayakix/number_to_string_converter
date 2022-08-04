fbfd28ab7982f2c3a752f793c32ba4d8979e0f8a
Iyana Marquez <yayakix@gmail.com>
2022-08-04 14:30:23 -0400

    1 through 99

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let inputNum = "9";
+let inputNum = "25";
+
+// decimals, maximum,
 
 const singles = [
   "zero",
@@ -26,11 +28,25 @@ const teens = [
   "nineteen",
 ];
 
-const doubles = []
+const doubles = ["", "", "twenty", "thirty", "fourty", "fifty", "sixty","seventy", "eighty", "ninety"]
 
-if (inputNum.length == 1){
-    console.log(singles[Number(inputNum)])
+const hundreds = ["hundred"]
+
+const thousands = ["thousand"]
+
+// check for single digits
+if (inputNum.length == 1) {
+  console.log(singles[Number(inputNum)]);
+} 
+// check for numbers between 10 and 19
+else if (inputNum.length == 2 && Number(inputNum) < 20) {
+  console.log(teens[Number(inputNum[1])]);
+} 
+// check for numbers between 20 and 99
+else if (inputNum.length == 2 && Number(inputNum) > 19) {
+  console.log(doubles[Number(inputNum[0])] + " " + singles[Number(inputNum[1])]);
 }
-else if (inputNum.length == 2){
-console.log(teens[Number(inputNum[1])])
+// check for numbers between 100 and 999
+else if (inputNum.length == 3){
+
 }
