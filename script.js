fb3f7bfb24a5d342ab9d607e388acc3561515662
Iyana Marquez <yayakix@gmail.com>
2022-08-04 16:11:27 -0400

    hundreds completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let inputNum = "25";
+let inputNum = "110";
 
 // decimals, maximum,
 
@@ -30,23 +30,59 @@ const teens = [
 
 const doubles = ["", "", "twenty", "thirty", "fourty", "fifty", "sixty","seventy", "eighty", "ninety"]
 
-const hundreds = ["hundred"]
+const hundreds = [" hundred "]
 
-const thousands = ["thousand"]
+const thousands = [" thousand "]
 
 // check for single digits
 if (inputNum.length == 1) {
   console.log(singles[Number(inputNum)]);
-} 
+}
 // check for numbers between 10 and 19
 else if (inputNum.length == 2 && Number(inputNum) < 20) {
   console.log(teens[Number(inputNum[1])]);
-} 
+}
 // check for numbers between 20 and 99
 else if (inputNum.length == 2 && Number(inputNum) > 19) {
-  console.log(doubles[Number(inputNum[0])] + " " + singles[Number(inputNum[1])]);
+  console.log(
+    doubles[Number(inputNum[0])] + " " + singles[Number(inputNum[1])]
+  );
 }
 // check for numbers between 100 and 999
-else if (inputNum.length == 3){
-
+else if (inputNum.length == 3) {
+  // check for 100 teen number
+  if (inputNum[1] == 1) {
+    console.log(
+      singles[Number(inputNum[0])] + hundreds[0] + teens[Number(inputNum[2])]
+    );
+  }
+  // if 0 in middle
+  else if (inputNum[1] == 0) {
+    console.log(
+      singles[Number(inputNum[0])] + hundreds[0] + singles[Number(inputNum[2])]
+    );
+  }
+  // if 0 at end
+  else if (inputNum[2] == 0) {
+    console.log("hi");
+    console.log(
+      singles[Number(inputNum[0])] + hundreds[0] + doubles[Number(inputNum[1])]
+    );
+  } else {
+    console.log(
+      singles[Number(inputNum[0])] +
+        hundreds[0] +
+        doubles[Number(inputNum[1])] +
+        " " +
+        singles[Number(inputNum[2])]
+    );
+  }
+} else if (inputNum.length == 4) {
+  console.log("a thousand number");
+} else if (inputNum.length == 5) {
+  console.log("a ten thousand number");
+} else if (inputNum.length == 6) {
+  console.log("a hundred thousand number");
+} else if (inputNum.length == 7) {
+  console.log("a million number");
 }
