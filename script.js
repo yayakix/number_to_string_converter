f305ede644b83e1440dcc041bcdf051366af65eb
Iyana Marquez <yayakix@gmail.com>
2022-08-04 13:39:37 -0400

    starting file

---
+++ script.js
@@ -0,0 +1,27 @@
+let inputNum = 3241;
+
+const singles = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+const teens = [
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
