30c4c14c882e35a4d94f51dab16ac955607047a9
Iyana Marquez <yayakix@gmail.com>
2022-08-08 19:24:51 -0400

    moved script to html

--- script.js
+++ script.js
@@ -1,138 +1,145 @@
-const singles = [
-  "",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-];
+// const singles = [
+//   "",
+//   "One",
+//   "Two",
+//   "Three",
+//   "Four",
+//   "Five",
+//   "Six",
+//   "Seven",
+//   "Eight",
+//   "Nine",
+// ];
 
-const teens = [
-  "ten",
-  "eleven",
-  "twelve",
-  "thirteen",
-  "fourteen",
-  "fifteen",
-  "sixteen",
-  "seventeen",
-  "eighteen",
-  "nineteen",
-];
+// const teens = [
+//   "Ten",
+//   "Eleven",
+//   "Twelve",
+//   "Thirteen",
+//   "Fourteen",
+//   "Fifteen",
+//   "Sixteen",
+//   "Seventeen",
+//   "Eighteen",
+//   "Nineteen",
+// ];
 
-const doubles = [
-  "",
-  "",
-  "twenty",
-  "thirty",
-  "fourty",
-  "fifty",
-  "sixty",
-  "seventy",
-  "eighty",
-  "ninety",
-];
+// const doubles = [
+//   "",
+//   "",
+//   "Twenty",
+//   "Thirty",
+//   "Fourty",
+//   "Fifty",
+//   "Sixty",
+//   "Seventy",
+//   "Eighty",
+//   "Ninety",
+// ];
 
-const hundreds = [" hundred "];
+// const hundreds = [" Hundred "];
 
-const thousands = [" thousand "];
+// const thousands = [" Thousand "];
 
-// function to calculate 3 digit numbers
-function findHundred(inputNum) {
-  if (inputNum[1] == 1) {
-    return (
-      singles[Number(inputNum[0])] + hundreds[0] + teens[Number(inputNum[2])]
-    );
-  }
-  // if 0 in middle ex. 103
-  else if (inputNum[1] == 0) {
-    return (
-      singles[Number(inputNum[0])] + hundreds[0] + singles[Number(inputNum[2])]
-    );
-  }
-  // if 0 at end / ex.110
-  else if (inputNum[2] == 0) {
-    return (
-      singles[Number(inputNum[0])] + hundreds[0] + doubles[Number(inputNum[1])]
-    );
-  } else {
-    return (
-      singles[Number(inputNum[0])] +
-      hundreds[0] +
-      doubles[Number(inputNum[1])] +
-      " " +
-      singles[Number(inputNum[2])]
-    );
-  }
-}
+// // function to calculate 3 digit numbers
+// function findHundred(inputNum) {
+//   if (inputNum[1] == 1) {
+//     return (
+//       singles[Number(inputNum[0])] + hundreds[0] + teens[Number(inputNum[2])]
+//     );
+//   }
+//   // if 0 in middle ex. 103
+//   else if (inputNum[1] == 0) {
+//     return (
+//       singles[Number(inputNum[0])] + hundreds[0] + singles[Number(inputNum[2])]
+//     );
+//   }
+//   // if 0 at end / ex.110
+//   else if (inputNum[2] == 0) {
+//     return (
+//       singles[Number(inputNum[0])] + hundreds[0] + doubles[Number(inputNum[1])]
+//     );
+//   } else {
+//     return (
+//       singles[Number(inputNum[0])] +
+//       hundreds[0] +
+//       doubles[Number(inputNum[1])] +
+//       " " +
+//       singles[Number(inputNum[2])]
+//     );
+//   }
+// }
 
-function findDoubles(inputNum) {
-  // check for numbers between 10 and 19
-  if (inputNum.length == 2 && Number(inputNum) < 20) {
-    return teens[Number(inputNum[1])];
-  }
-  // check for numbers between 20 and 99
-  else if (inputNum.length == 2 && Number(inputNum) > 19) {
-    return doubles[Number(inputNum[0])] + " " + singles[Number(inputNum[1])];
-  }
-}
-function findThousand(inputNum) {
-  // 1111
-  if (inputNum[1] != 0) {
-    return (
-      singles[Number(inputNum[0])] +
-      " thousand " +
-      findHundred(inputNum.slice(1))
-    );
-  }
-  // 1011
-  else if (inputNum[1] == 0 && inputNum[2] != 0) {
-    return (
-      singles[Number(inputNum[0])] +
-      " thousand " +
-      findDoubles(inputNum.slice(2))
-    );
-  }
-  // 1001
-  else if (inputNum[1] == 0 && inputNum[2] == 0) {
-    return (
-      singles[Number(inputNum[0])] +
-      " thousand " +
-      singles[Number(inputNum.slice(3))]
-    );
-  }
-}
+// function findDoubles(inputNum) {
+//   // check for numbers between 10 and 19
+//   if (inputNum.length == 2 && Number(inputNum) < 20) {
+//     return teens[Number(inputNum[1])];
+//   }
+//   // check for numbers between 20 and 99
+//   else if (inputNum.length == 2 && Number(inputNum) > 19) {
+//     return doubles[Number(inputNum[0])] + " " + singles[Number(inputNum[1])];
+//   }
+// }
+// function findThousand(inputNum) {
+//   // 1111
+//   if (inputNum[1] != 0) {
+//     return (
+//       singles[Number(inputNum[0])] +
+//       " Thousand " +
+//       findHundred(inputNum.slice(1))
+//     );
+//   }
+//   // 1011
+//   else if (inputNum[1] == 0 && inputNum[2] != 0) {
+//     return (
+//       singles[Number(inputNum[0])] +
+//       " Thousand " +
+//       findDoubles(inputNum.slice(2))
+//     );
+//   }
+//   // 1001
+//   else if (inputNum[1] == 0 && inputNum[2] == 0) {
+//     return (
+//       singles[Number(inputNum[0])] +
+//       " Thousand " +
+//       singles[Number(inputNum.slice(3))]
+//     );
+//   }
+// }
 
+// function stringtoNum(inputNum) {
+//   // check for single digits
+//   if (inputNum.length == 1) {
+//     return singles[Number(inputNum)];
+//   } else if (inputNum.length == 2) {
+//     return findDoubles(inputNum);
+//   }
+//   // check for numbers between 100 and 999
+//   else if (inputNum.length == 3) {
+//     return findHundred(inputNum);
+//     // check for 100 teen number
+//   }
+//   // check thousands place
+//   else if (inputNum.length == 4) {
+//     return findThousand(inputNum);
+//   }
+// }
 
-function stringtoNum(inputNum) {
-  // check for single digits
-  if (inputNum.length == 1) {
-    return singles[Number(inputNum)];
+// // 1000
+// // 1011
+// // 1001
+// // 1100
+// // 1110
+// // 1111
 
-  } else if (inputNum.length == 2) {
-    return findDoubles(inputNum);
-  }
-  // check for numbers between 100 and 999
-  else if (inputNum.length == 3) {
-    return findHundred(inputNum);
-    // check for 100 teen number
-  }
-  // check thousands place
-  else if (inputNum.length == 4) {
-    return findThousand(inputNum);
-  }
-}
+// // console.log(stringtoNum("5679"));
 
-// 1000
-// 1011
-// 1001
-// 1100
-// 1110
-// 1111
-let inputNum = "199";
-val = stringtoNum(inputNum);
-console.log(stringtoNum(inputNum));
+// const input = document.getElementById("input");
+// const button = document.getElementById("button");
+// const answer = document.getElementById("answer");
+
+// button.addEventListener("click", function () {
+//    convertedNum = stringtoNum(input.value)
+//   answer.textContent = convertedNum
+// });
+
